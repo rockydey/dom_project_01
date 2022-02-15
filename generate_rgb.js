9bad932bed7dcc6f52bec6b0d68c35fd73e773a6
rockydey <coderrocky25@gmail.com>
2022-02-15 20:04:52 +0600

    trying different possibilities using js

--- generate_rgb.js
+++ generate_rgb.js
@@ -10,9 +10,19 @@ function main() {
 }
 
 function generateRGBColor() {
-    const red = Math.floor(Math.random() * 256);
+    /* const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`; */
 
-    return `rgb(${red}, ${green}, ${blue})`;
+    // remains same color code
+    /* const color = Math.floor(Math.random() * 256);
+    return `rgb(${color}, ${color}, ${color})` */
+
+    // calling another function for color code
+    return `rgb(${returnColor()}, ${returnColor()}, ${returnColor()})`
+}
+
+function returnColor() {
+    return Math.floor(Math.random() * 256);
 }
